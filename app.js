70b73c0cb5b240d7cb78e636006efcac0e654b85
Salko <64722632+sarajlija@users.noreply.github.com>
2021-02-07 13:24:18 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const toggleImg = document.querySelector(".icon__menu");
+const socialMenu = document.querySelector(".social-menu-open");
+
+toggleImg.addEventListener("click", function () {
+    
+    socialMenu.classList.toggle("close-social-menu");
+   
+    });
+    
+
+
+
+    
